fd5e3e366fbe928fa7dedc9fa7c4219772d48e9e
Joel Teply <joel@cambriantech.com>
2021-06-13 14:29:51 -0500

    working script

--- scripts/create_samples.sh
+++ scripts/create_samples.sh
@@ -1,14 +1,19 @@
 
 #following example and some data from https://memememememememe.me/post/training-haar-cascades/
 
-cd negatives
-rm -f negatives.txt
-ls -l1 *.jpg > negatives.txt
-cd ../
+rm -f negatives/negatives.txt
+ls -l1 negatives/*.jpg > negatives.txt
 
-opencv_createsamples -img cropped00.jpg \
--bg negatives/negatives.txt \
--info samples/cropped00.txt \
--num 128 -maxxangle 0.0 -maxyangle 0.0 \
--maxzangle 0.3 -bgcolor 255 -bgthresh 8 \
--w 48 -h 48
+CV_APP_PATH=./bin/darwin
+
+#apps built statically from opencv source: 
+#cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_opencv_apps=ON -DBUILD_SHARED_LIBS=OFF ../opencv
+
+rm -rf samples/samples.txt
+mkdir -p samples
+
+${CV_APP_PATH}/opencv_createsamples \
+-img positives/cropped00.jpg \
+-bg negatives.txt \
+-info samples/samples.txt \
+-num 128 -w 48 -h 30
