935f337e69f4a61ff934a02c1c622fdfe0001d73
Joel Teply <joel@cambriantech.com>
2021-06-13 19:48:52 -0500

    works

--- scripts/create_samples.sh
+++ scripts/create_samples.sh
@@ -1,8 +1,12 @@
 
 #following example and some data from https://memememememememe.me/post/training-haar-cascades/
 
+#exiftool -q -r -if '$ImageHeight < 480' -if '$ImageWidth < 640' -p '$Directory/$FileName' "negatives" | xargs rm
+
 rm -f negatives/negatives.txt
-ls -l1 negatives/*.jpg > negatives.txt
+cd negatives
+ls -l1 *.jpg > negatives.txt
+cd ..
 
 CV_APP_PATH=./bin/darwin
 
@@ -15,25 +19,39 @@ mkdir -p samples
 index=0
 gen=128
 
+#find "negatives" -iname "*.jpg" -type f | xargs -I{} identify -format '%w %h %i' {} | awk '$1<640 || $2<480'
+
 find "positives" \( -iname \*.jpg -o -iname \*.jpeg \) -print0 | while read -r -d $'\0' file; do
   # base="${file##*/}" $base is the file name with all the directory stuff stripped off
   # dir="${file%/*}    $dir is the directory with the file name stripped off
   # echo "$file"
-  IMG_PATH="${file}"
-  INFO_PATH=samples/samples_${index}.txt
+  INFO_PATH="samples/samples_${index}.txt"
 
-  echo "Processing ${IMG_PATH}"
+  echo "Processing image ${file}"
+
+  	seed=$((10000 + $RANDOM % 10000))
 
 	${CV_APP_PATH}/opencv_createsamples \
+		-rngseed ${seed} \
 		-img ${file} \
-		-bg negatives.txt \
+		-bg negatives/negatives.txt \
 		-info ${INFO_PATH} \
-		-num ${gen} -maxxangle 0.0 -maxyangle 0.0 \
-		-maxzangle 0.3 -bgcolor 255 -bgthresh 8 \
-		-w 48 -h 30
+		-num ${gen} \
+		-maxxangle 0.3 -maxyangle 0.4 -maxzangle 0.4 \
+		-w 128 -h 80
 
 	if [[ -f "$INFO_PATH" ]]; then
 		index=`expr $index + 1`
 	fi
 
 done
+
+cat samples/samples_*.txt > samples/samples.txt
+
+opencv_createsamples \
+	-info samples/samples.txt \
+	-bg negatives/negatives.txt \
+	-vec cropped.vec \
+	-num 1920 -w 128 -h 80
+
+
